d4b3c97eec401a8f42d122fd51e8808ba8cfb406
kasiasurtel <surtel.katarzyna@gmail.com>
2023-01-28 18:02:07 +0100

    Gradle & project updates #3

--- app/src/main/java/com/ks/trackmytag/MainActivity.kt
+++ app/src/main/java/com/ks/trackmytag/MainActivity.kt
@@ -7,15 +7,19 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.preferencesDataStore
+import com.ks.trackmytag.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "app_preferences")
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
